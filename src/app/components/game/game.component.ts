7b442296c0ff4a54cd93072f61ee68d43a27a03a
abrych <arkadiusz.brych@external.t-mobile.pl>
2020-04-04 19:54:32 +0200

    Initial commit

---
+++ src/app/components/game/game.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+
+  messageForUser: string;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  sayHello(value: string) {
+    this.messageForUser = 'cześć ' + value;
+  }
+}
